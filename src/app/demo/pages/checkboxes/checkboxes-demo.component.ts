29148a421c8b43a97a46e8641764008f9cb7ecc6
ps-fperez <fperez@pack-solutions.com>
2026-02-24 15:12:32 +0100

    Updated tsconfig.json

--- src/app/demo/pages/checkboxes/checkboxes-demo.component.ts
+++ src/app/demo/pages/checkboxes/checkboxes-demo.component.ts
@@ -48,5 +48,83 @@ export class CheckboxesDemoComponent {
     this.termsControl.setValue(!this.termsControl.value);
   }
 
-  // Codes snippets (ommis pour la clarté, garde les tiens tels quels)
+  labelRightCode = `<psh-checkbox
+  [(checked)]="isChecked"
+  labelPosition="right"
+>
+  Label à droite
+</psh-checkbox>`;
+
+  labelLeftCode = `<psh-checkbox
+  [(checked)]="isChecked"
+  labelPosition="left"
+>
+  Label à gauche
+</psh-checkbox>`;
+
+  noLabelCode = `<psh-checkbox
+  [(checked)]="isChecked"
+  ariaLabel="Sélectionner l'élément"
+/>`;
+
+  disabledCode = `<psh-checkbox
+  [disabled]="true"
+  [(checked)]="isChecked"
+>
+  Option désactivée
+</psh-checkbox>`;
+
+  errorCode = `<psh-checkbox
+  [error]="'Champ requis'"
+  [(checked)]="isChecked"
+>
+  Option avec erreur
+</psh-checkbox>`;
+
+  successCode = `<psh-checkbox
+  [checked]="true"
+  [success]="'Sélection valide'"
+>
+  Option validée
+</psh-checkbox>`;
+
+  indeterminateCode = `<psh-checkbox
+  [indeterminate]="true"
+>
+  Sélection partielle
+</psh-checkbox>`;
+
+  reactiveFormsCode = `import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { PshCheckboxComponent } from 'ps-helix';
+
+@Component({
+  imports: [PshCheckboxComponent, ReactiveFormsModule],
+  template: \`
+    <psh-checkbox
+      [formControl]="termsControl"
+      label="J'accepte les conditions"
+    />
+  \`
+})
+export class MyComponent {
+  termsControl = new FormControl(false);
+}`;
+
+  signalFormsCode = `import { signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
+import { PshCheckboxComponent } from 'ps-helix';
+
+@Component({
+  imports: [PshCheckboxComponent, FormField],
+  template: \`
+    <psh-checkbox
+      [formField]="settingsForm.terms"
+      label="J'accepte les conditions"
+    />
+  \`
+})
+export class MyComponent {
+  settingsModel = signal({ terms: false });
+  settingsForm = form(this.settingsModel);
+}`;
 }
