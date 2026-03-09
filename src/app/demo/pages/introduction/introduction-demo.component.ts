0b71616cb180e6cdd769305b21ee3c107542c9f0
ps-fperez <fperez@pack-solutions.com>
2026-03-09 15:11:34 +0100

    Updated introduction-demo.component.ts

--- src/app/demo/pages/introduction/introduction-demo.component.ts
+++ src/app/demo/pages/introduction/introduction-demo.component.ts
@@ -11,7 +11,7 @@ import { CodeSnippetComponent } from '../../shared/code-snippet.component';
 })
 export class IntroductionDemoComponent {
   npmUrl = 'https://www.npmjs.com/package/ps-helix';
-  npmVersion = '4.0.5';
+  npmVersion = '4.0.6';
   keyFeatures = [
     {
       icon: 'lightning',
